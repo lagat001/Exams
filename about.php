1d999cd9ad30301751248b1cb7da64c552b10c09
Mike Lagat <123958334+lagat001@users.noreply.github.com>
2023-06-23 14:35:07 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>O. E. M | About </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Minimal Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<script src="js/jquery.min.js"> </script>
+<script src="js/bootstrap.min.js"> </script>
+<style>
+.fw{min-width:100% !important;}
+.left{float:left !important; height:450px;}
+</style>
+</head>
+<body>
+<div class="navbar-header fw"> 
+       <h1> <a class="navbar-brand fw" href="index.php">ONLINE EXAMS MANAGEMENT SYSTEM</a></h1> 
+            <div class="clearfix"> </div>
+	   
+</div>
+	<div class="four">
+		<p>
+		            <div class="clearfix"> </div>
+
+        <a href="index.php" class="x"><button type="button" class="btn btn-lg btn-default">Home</button></a>
+        <a href="about.php" class="x"><button type="button" class="btn btn-lg btn-default">About</button></a>
+        <a href="#" class="x"><button type="button" class="btn btn-lg btn-default">Alerts</button></a>
+        <a href="#" class="x"><button type="button" class="btn btn-lg btn-default">About</button></a>
+      </p>
+<div class="col-md-4 left">aa</div>
+<div class="col-md-4 left">aa</div>
+<div class="col-md-4 left">aa</div>
+		<p><b>O. E. M. S</b> ( ONLINE EXAMS MANAGEMENT SYSTEM )</p>
+		<a href="login.php" class="hvr-shutter-in-horizontal">LOGIN</a>
+	</div>
+		<!---->
+<div class="copy-right">
+            <p> &copy; 2023 </p>	    </div>
+		
+     
+<!---->
+<!--scrolling js-->
+	<script src="js/jquery.nicescroll.js"></script>
+	<script src="js/scripts.js"></script>
+	<!--//scrolling js-->
+</body>
+</html>
+
---
+++ index1.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>E.M.S | Home </title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Minimal Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<script src="js/jquery.min.js"> </script>
+<script src="js/bootstrap.min.js"> </script>
+<style>
+.fw{min-width:100% !important;}
+.four a{display: inline !important;}
+</style>
+</head>
+<body>
+<div class="navbar-header fw"> 
+       <h1> <a class="navbar-brand fw" href="index.php">EXAMINATIONS MANAGEMENT SYSTEM</a>
+       </h1> 
+            <div class="clearfix"> </div>
+	   
+</div>
+	<div class="four">
+		<p>
+		            <div class="clearfix"> </div>
+
+        <a href="index.php" class="hvr-shutter-in-horizontal">HOME</a>
+        <a href="about.php" class="hvr-shutter-in-horizontal">ABOUT</a>
+		<a href="login.php" class="hvr-shutter-in-horizontal">LOGIN</a>
+      </p>
+<!--	<img src="images/404.png" alt="" />-->	
+		<p><b>E.M.S</b> ( EXAMINATION MANAGEMENT SYSTEM )</p>
+	</div>
+		<!---->
+<div class="copy-right">
+            <p> &copy; 2022 E.M.S | Design by Mike Sergon </p>	    </div>
+		
+     
+<!---->
+<!--scrolling js-->
+	<script src="js/jquery.nicescroll.js"></script>
+	<script src="js/scripts.js"></script>
+	<!--//scrolling js-->
+</body>
+</html>
+
